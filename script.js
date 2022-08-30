89fcd839635e0d20752e968bdfcb4a790d414164
Naseeb Panghal <naseebsingh3510@gmail.com>
2022-08-30 13:45:17 +0530

    Fixed Ui and Buttons

--- script.js
+++ script.js
@@ -1,136 +1,37 @@
-function num1(){
-    const getInputValue = document.querySelector("#inputNumbersID");
-    getInputValue.value += 1;
-    console.log(getInputValue.value);
-}
-function num2(){
-    const getInputValue = document.querySelector("#inputNumbersID");
-    getInputValue.value += 2;
-    console.log(getInputValue.value);
-}
-function num3(){
-    const getInputValue = document.querySelector("#inputNumbersID");
-    getInputValue.value += 3;
-    console.log(getInputValue.value);
-}
-function num4(){
-    const getInputValue = document.querySelector("#inputNumbersID");
-    getInputValue.value += 4;
-    console.log(getInputValue.value);
-}
-function num5(){
-    const getInputValue = document.querySelector("#inputNumbersID");
-    getInputValue.value += 5;
-    console.log(getInputValue.value);
-}
-function num6(){
-    const getInputValue = document.querySelector("#inputNumbersID");
-    getInputValue.value += 6;
-    console.log(getInputValue.value);
-}
-function num7(){
-    const getInputValue = document.querySelector("#inputNumbersID");
-    getInputValue.value += 7;
-    console.log(getInputValue.value);
-}
-function num8(){
-    const getInputValue = document.querySelector("#inputNumbersID");
-    getInputValue.value += 8;
-    console.log(getInputValue.value);
-}
-function num9(){
-    const getInputValue = document.querySelector("#inputNumbersID");
-    getInputValue.value += 9;
-    console.log(getInputValue.value);
-}
-function num0(){
-    const getInputValue = document.querySelector("#inputNumbersID");
-    getInputValue.value += 0;
-    console.log(getInputValue.value);
-}
-function add(){
-    const getInputValue = document.querySelector("#inputNumbersID");
-    getInputValue.value += "+";
-    console.log(getInputValue.value);
-}
-function sub(){
-    const getInputValue = document.querySelector("#inputNumbersID");
-    getInputValue.value += "-";
-    console.log(getInputValue.value);
-}
-function mul(){
-    const getInputValue = document.querySelector("#inputNumbersID");
-    getInputValue.value += "*";
-    console.log(getInputValue.value);
-}
-function div(){
-    const getInputValue = document.querySelector("#inputNumbersID");
-    getInputValue.value += "/";
-    console.log(getInputValue.value);
-}
+const getInputValue = document.querySelector("#inputNumbersID");
 
-// function result(){
-//     const getInputValue = document.querySelector("#inputNumbersID");
-//     let resultString = getInputValue.value;
-//     resultString = resultString.match(/(\d+|[^\d]+)/g);
-//     // console.log(resultString);
-
-//     function calCulator(...resultString){
-//         let storeFirstNum = parseInt(resultString[0]);
-//         let storeSecondNum = parseInt(resultString[2]);
-//         let storeOp = resultString[1];
-//         let finalResult = 0;
-
-//         console.log();
-
-//         switch(storeOp){
-//             case "+": {
-//                 finalResult = storeFirstNum + storeSecondNum;
-//                 getInputValue.value = " ";
-//                 getInputValue.value = finalResult;
-//                 console.log(finalResult);
-//                 break;
-//             }
-//             case "-": {
-//                 finalResult = storeFirstNum - storeSecondNum;
-//                 getInputValue.value = " ";
-//                 getInputValue.value = finalResult;
-//                 console.log(finalResult);
-//                 break;
-//             }
-//             case "*": {
-//                 finalResult = storeFirstNum * storeSecondNum;
-//                 getInputValue.value = " ";
-//                 getInputValue.value = finalResult;
-//                 console.log(finalResult);
-//                 break;
-//             }
-//             case "/": {
-//                 finalResult = storeFirstNum / storeSecondNum;
-//                 getInputValue.value = " ";
-//                 getInputValue.value = finalResult;
-//                 console.log(finalResult);
-//                 break;
-//             }
-//         }
-//     }
-//     calCulator(...resultString);
-// }
-
-function reset(){
-    const getInputValue = document.querySelector("#inputNumbersID");
-    getInputValue.value = "";
-}
+const num1 =  () => getInputValue.value += 1;
+const num2 =  () => getInputValue.value += 2;
+const num3 =  () => getInputValue.value += 3;
+const num4 =  () => getInputValue.value += 4;
+const num5 =  () => getInputValue.value += 5;
+const num6 =  () => getInputValue.value += 6;
+const num7 =  () => getInputValue.value += 7;
+const num8 =  () => getInputValue.value += 8;
+const num9 =  () => getInputValue.value += 9;
+const num0 =  () => getInputValue.value += 0;
+const add = () => getInputValue.value += "+";
+const sub = () => getInputValue.value += "-";
+const mul = () => getInputValue.value += "*";
+const div = () => getInputValue.value += "/";
+const numDot = () => getInputValue.value += ".";
 
 
 
-function result(){
-    const getInputValue = document.querySelector("#inputNumbersID");
-    let resultString = getInputValue.value;
+let fetchExpBox = document.querySelector(".expBox");
+function result() {
 
-    let finalResultString = resultString.replace(/[&\=\\#,()$~'":?<>{}]/g, '')
+    let finalResultString = (getInputValue.value).replace(/[&\=\\#,()$~'":?<>{}]/g, '');
 
-    console.log(finalResultString);
-    getInputValue.value = `${finalResultString} = ${eval(finalResultString)}`
+    getInputValue.value = `${eval(finalResultString)}`;
 
+    function setlabel(finalResultString) {
+        fetchExpBox.innerText = `${finalResultString} = `;
+    }
+    setlabel(finalResultString);
+}
+
+function reset() {
+    getInputValue.value = ""; 
+    fetchExpBox.innerText = "";
 }
